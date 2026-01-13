f9ba1af2a4a7c75ce1074a00062d7af773bcc8c7
Trousdale <ktrousdale4@una.edu>
2026-01-13 13:11:33 -0600

    commit message

---
+++ app.js
@@ -0,0 +1,18 @@
+//app.js
+
+//es5 import express from 'express'
+import { express } from 'express';
+//
+//const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!!')
+})
+
+
+//server start
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
