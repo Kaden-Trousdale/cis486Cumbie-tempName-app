414406bc82c209a8563791fa5bab4d3ac57ba7c2
Kaden Trousdale <Kaden.Trousdale@gmail.com>
2026-01-19 21:26:47 -0600

    Define PORT for server configuration

--- app.mjs
+++ app.mjs
@@ -7,7 +7,7 @@ import express from 'express'
 //const express = require('express');
 
 const app = express()
-
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!!!')
 })
