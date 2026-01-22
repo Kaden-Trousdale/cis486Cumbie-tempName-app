4f30045b604e1a0d2b5399be06fc6da3be059f2d
Trousdale <ktrousdale4@una.edu>
2026-01-22 13:17:37 -0600

    New

--- app.mjs
+++ app.mjs
@@ -2,14 +2,22 @@
 
 //es5 import express from 'express'
 import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 //
 //const express = require('express');
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const PORT = process.env.PORT || 3000;
+
+// middlewares (get to slash) = "http verb" to slash "Endpoint name"
 app.get('/', (req, res) => {
-  res.send('Hello World!!!')
+ // res.send('Hello World!!! updated');//string response
+ // res.sendFile('index.html');
+  res.sendFile(join(__dirname, 'public', 'index.html'));
 })
 
 
