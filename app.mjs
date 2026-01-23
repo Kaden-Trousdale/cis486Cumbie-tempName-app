9ccf7cf6ece8d19f70ec29f4ccbd2bb9d6dde4c7
Kaden Trousdale <Kaden.Trousdale@gmail.com>
2026-01-23 17:27:04 -0600

    Refactor app.mjs to use ES6 imports and add routes

--- app.mjs
+++ app.mjs
@@ -1,27 +1,59 @@
-//app.js
-
-//es5 import express from 'express'
-import express from 'express'
+//app.mjs
+//we are in ES6, use this. 
+import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+// import * as fs from 'node:fs';
 
-//
-//const express = require('express');
-
-const app = express()
+const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const PORT = process.env.PORT || 3000;
+// const files = fs.readFile('.');
+// let myVar = 'demo purposes only';
 
-// middlewares (get to slash) = "http verb" to slash "Endpoint name"
+app.use(express.json()); 
+
+// middlewares aka endpoints aka 'get to slash' {http verb} to slash {you name ur endpoint}
 app.get('/', (req, res) => {
- // res.send('Hello World!!! updated');//string response
- // res.sendFile('index.html');
-  res.sendFile(join(__dirname, 'public', 'index.html'));
+  //res.send('Hello Express'); //string response
+  //res.sendFile('index.html'); // <- this don't work w/o imports, assign, and arguements
+  res.sendFile(join(__dirname, 'public', 'index.html')) ;
+
 })
 
+// app.get('/inject', (req, res) => {
+//   // Inject a server variable into barry.html
+//   fs.files(join(__dirname, 'public', 'index.html'), 'utf8')
+//     .then(html => {
+//       // Replace a placeholder in the HTML (e.g., {{myVar}})
+//       const injectedHtml = html.replace('{{myVar}}', myVar);
+//       res.send(injectedHtml);
+//     })
+//     .catch(err => {
+//       res.status(500).send('Error loading page');
+//     });
+// })
 
-//server start
+app.get('/api/json', (req, res) =>{
+  const myVar = 'Hello from server!';
+  res.json({ myVar });
+})
+
+app.get('/api/query', (req, res) => {
+
+  console.log("client request with query param:", req.query.name); 
+  res.json({"name": req.query.name});
+});
+
+app.post('/api/body', (req, res) => {
+  console.log("the body:", req.body); 
+  console.log("client request with body:", req.body.name); 
+  res.json({"name": req.body.name});
+});
+
+
+
+//start the server. 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
